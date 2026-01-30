1ae624118fe78f87b4565ddda8b488257b5d186a
Amith Abraham <amith.abraham@hpe.com>
2026-01-30 11:09:37 -0600

    cxi_vm script fixes
    - Use $CWD instead of hardcoded workspace paths in SSH commands

--- cxi_vm_commit.sh
+++ cxi_vm_commit.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+CWD=$PWD
 make -j8
 # We need a TTY, otherswise the tests will not finish. So ssh to self.
-ssh -tt localhost "cd ~/workspace/workspace/os-networking-team/cassini-vm/cxi-driver/; make check"
+ssh -tt localhost "cd $CWD && make check"
--- cxi_vm_pr.sh
+++ cxi_vm_pr.sh
@@ -6,4 +6,4 @@ git rebase `git merge-base origin/main HEAD` --exec 'set -e; git log -1 && make 
 
 # Then build and test
 # We need a TTY, otherswise the tests will not finish. So ssh to self.
-git rebase `git merge-base origin/main HEAD` --exec 'set -e; git log -1 && make clean && make -j8 && ssh -tt localhost "cd ~/workspace/workspace/os-networking-team/cassini-vm/PR-cxi-driver/; make check"'
+git rebase `git merge-base origin/main HEAD` --exec 'set -e; CWD=$PWD; git log -1 && make clean && make -j8 && ssh -tt localhost "cd $CWD && make check"'
