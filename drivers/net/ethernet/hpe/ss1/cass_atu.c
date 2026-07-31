HewlettPackard/shs-cxi-driver

commit 938633a4e81db3ab4213b9d603e783ab85c93884

sriov: Add VF support for LNI, services, and communication profiles
Implement SR-IOV Virtual Function (VF) support to enable VF devices to
allocate and manage LNIs, services, and communication profiles through
the Physical Function (PF).

Changes include:

VF-specific function implementations:
- Add VF versions of core allocation functions (_vf variants)
- Redirect operations to PF when invoked from VF context
- Introduce VF private structures (cxi_lni_priv_vf, cxi_cp_priv_vf)

Communication infrastructure:
- Extend cxi_user_core to handle commands from both user space and VF
  clients, supporting both PF and VF execution contexts
- Add cxi_vf_cmd.h with extended command/response structures for VF
  operations without modifying the existing cxi-abi.h interface used
  by libfabric
- Update internal headers to expose necessary functions for VF support

Address context configuration:
- Configure PI VF fields in address contexts to ensure the NIC uses
  the correct PCI VF when accessing memory

Security restrictions:
- Remove VF permissions for privileged DMAC and SBUS operations

drivers/net/ethernet/hpe/ss1/cass_atu.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0
-/* Copyright 2018 Hewlett Packard Enterprise Development LP */
+/* Copyright 2018, 2024-2026 Hewlett Packard Enterprise Development LP */
 
 /* Address Translation Unit (ATU) management */
 
@@ -87,16 +87,17 @@ void cass_atu_fini(struct cass_dev *hw)
 
 /* Atomically write an Address Context */
 static void cass_write_ac(struct cass_dev *hw, union c_atu_cfg_ac_table *ac,
-			  unsigned int index, bool user)
+			  unsigned int index, bool user, bool is_physac,
+			  bool vf_en, int vf_num)
 {
 	union c_pi_cfg_acxt pi_cfg_acxt = {
 		.ac_en = ac->context_en,
 	};
-	struct cass_ac *cac = container_of(ac, struct cass_ac, cfg_ac);
+
+	pi_cfg_acxt.vf_en = vf_en;
+	pi_cfg_acxt.vf_num = vf_num;
 
 	if (ac->ta_mode != C_ATU_NTA_MODE) {
-		pi_cfg_acxt.vf_en = ac->ats_vf_en;
-		pi_cfg_acxt.vf_num = ac->ats_vf_num;
 		pi_cfg_acxt.pasid = ac->ats_pasid;
 		pi_cfg_acxt.pasid_en = ac->ats_pasid_en;
 		pi_cfg_acxt.pasid_er = ac->ats_pasid_er;
@@ -129,8 +130,11 @@ static void cass_write_ac(struct cass_dev *hw, union c_atu_cfg_ac_table *ac,
 	/* WORKAROUND: Cassini ERRATA-3277
 	 * When disabling an AC, invalidate before disabling the ACXT.
 	 */
-	if (cass_version(hw, CASSINI_1) && !ac->context_en)
+	if (!is_physac && cass_version(hw, CASSINI_1) && !ac->context_en) {
+		struct cass_ac *cac = container_of(ac, struct cass_ac, cfg_ac);
+
 		cass_invalidate_range(cac, 0, ATUCQ_INVALIDATE_ALL);
+	}
 
 	cass_config_pi_acxt(hw, index, &pi_cfg_acxt);
 }
@@ -206,8 +210,8 @@ int cass_atu_init(struct cass_dev *hw)
 	for (i = 0; i < FBINS; i++)
 		hw->pri_fault_time[i] = 0;
 
-
-	cass_write_ac(hw, &ac, ATU_PHYS_AC, false);
+	cass_write_ac(hw, &ac, ATU_PHYS_AC, false, false,
+		      ac.ats_vf_en, ac.ats_vf_num);
 	cass_write(hw, C_ATU_CFG_NTA, &cfg_nta, sizeof(union c_atu_cfg_nta));
 
 	hw->oxe_dummy_dma_addr = dma_map_single(&hw->cdev.pdev->dev,
@@ -362,6 +366,51 @@ static void cass_lac_free(struct cxi_lni_priv *lni_priv, int lac)
 }
 
 /**
+ * cass_ac_phys_alloc - Allocate a physical Address Context for a VF
+ *
+ * This per-VF AC is analogous to ATU_PHYS_AC and is used in its place when
+ * configuring resources such as CQs that belong to a VF.
+ *
+ * @hw: CXI device
+ * @vf_en: true if allocating an AC for a VF
+ * @vf_num: VF number to allocate AC for (ignored if vf_en is false)
+ */
+int cass_ac_phys_alloc(struct cass_dev *hw, bool vf_en, int vf_num)
+{
+	int acid;
+	union c_atu_cfg_ac_table ac = {
+		.context_en = 1,
+		.ta_mode = C_ATU_PASSTHROUGH_MODE,
+		.mem_size = ~0,
+		.ats_vf_en = vf_en,
+		.ats_vf_num = vf_en ? vf_num : 0,
+	};
+
+	acid = ida_simple_get(&hw->atu_table, 1, ATU_PHYS_AC, GFP_KERNEL);
+	if (acid < 0)
+		return acid;
+
+	cass_write_ac(hw, &ac, acid, false, true, vf_en, vf_num);
+
+	return acid;
+}
+
+/**
+ * cass_ac_phys_free - Free a physical Address Context allocated by
+ * cass_ac_phys_alloc()
+ *
+ * @hw: CXI device
+ * @acid: ID of address context
+ */
+void cass_ac_phys_free(struct cass_dev *hw, int acid)
+{
+	union c_atu_cfg_ac_table ac = {};
+
+	cass_write_ac(hw, &ac, acid, false, true, false, 0);
+	ida_simple_remove(&hw->atu_table, acid);
+}
+
+/**
  * cass_ac_alloc() - Allocate an Address Context
  *
  * Once the ACID is written to hardware, it cannot be freed until the LNI
@@ -435,7 +484,9 @@ static struct cass_ac *cass_ac_alloc(struct cxi_lni_priv *lni_priv,
 	hw->cac_table[cac->ac.acid] = cac;
 
 	cass_write_ac(hw, &cac->cfg_ac, cac->ac.acid,
-		      m_opts->flags & CXI_MAP_USER_ADDR);
+		      m_opts->flags & CXI_MAP_USER_ADDR, false,
+		      cac->lni_priv->is_vf,
+		      cac->lni_priv->is_vf ? cac->lni_priv->vf_num : 0);
 	cass_set_acid(hw, lni_priv->lni.rgid, cac->ac.lac, cac->ac.acid);
 
 	INIT_LIST_HEAD(&cac->md_list);
@@ -485,7 +536,8 @@ static void cass_ac_free(struct cass_dev *hw, struct cass_ac *cac)
 	}
 
 	memset(&cac->cfg_ac, 0, sizeof(cac->cfg_ac));
-	cass_write_ac(hw, &cac->cfg_ac, cac->ac.acid, false);
+	cass_write_ac(hw, &cac->cfg_ac, cac->ac.acid, false, false,
+		      lni_priv->is_vf, lni_priv->is_vf ? lni_priv->vf_num : 0);
 
 	cass_unbind_ac(hw, cac);
 
@@ -511,7 +563,9 @@ void cass_acs_disable(struct cxi_lni_priv *lni_priv)
 
 	list_for_each_entry(cac, &lni_priv->ac_list, list) {
 		cac->cfg_ac.context_en = 0;
-		cass_write_ac(hw, &cac->cfg_ac, cac->ac.acid, false);
+		cass_write_ac(hw, &cac->cfg_ac, cac->ac.acid, false, false,
+			      cac->lni_priv->is_vf,
+			      cac->lni_priv->is_vf ? cac->lni_priv->vf_num : 0);
 	}
 
 	mutex_unlock(&lni_priv->ac_list_mutex);
@@ -1019,23 +1073,41 @@ void cass_align_start_len(struct ac_map_opts *m_opts, uintptr_t va, size_t len,
 }
 
 /**
- * cxi_phys_lac_alloc() - Map an AC into a resource group for use in DMA
- *                        commands. Exported to allow clients to perform
- *                        DMA using physical addresses.
+ * cxi_phys_lac_alloc_vf() - Allocate a physical LAC for the given LNI for a VF.
+ *
+ * @lni: the Logical Network Interface
+ * @return: LAC or negative errno
+ */
+static int cxi_phys_lac_alloc_vf(struct cxi_lni *lni)
+{
+	struct cxi_lni_priv_vf *lni_priv_vf = container_of(lni, struct cxi_lni_priv_vf, lni);
+
+	return lni_priv_vf->phys_lac;
+}
+
+/**
+ * cxi_phys_lac_alloc() - Allocate a physical LAC for the given LNI.
+ *                        Exported to allow clients to perform DMA using physical addresses.
+ *
  * @lni: the Logical Network Interface
  * @return: LAC or negative errno
  */
 int cxi_phys_lac_alloc(struct cxi_lni *lni)
 {
 	struct cxi_lni_priv *lni_priv = container_of(lni, struct cxi_lni_priv, lni);
+	struct cxi_dev *cdev = lni_priv->dev;
 	struct cass_dev *hw = container_of(lni_priv->dev, struct cass_dev, cdev);
 	int lac;
+
+	if (!cdev->is_physfn)
+		return cxi_phys_lac_alloc_vf(lni);
 
 	lac = cass_lac_get(hw, lni_priv->lni.rgid);
 	if (lac < 0)
 		return lac;
 
-	cass_set_acid(hw, lni_priv->lni.rgid, lac, ATU_PHYS_AC);
+	/* Program the phys_ac that was already selected at the lni allocation */
+	cass_set_acid(hw, lni_priv->lni.rgid, lac, lni_priv->phys_ac);
 
 	return lac;
 }
@@ -1050,6 +1122,13 @@ EXPORT_SYMBOL(cxi_phys_lac_alloc);
 void cxi_phys_lac_free(struct cxi_lni *lni, int lac)
 {
 	struct cxi_lni_priv *lni_priv = container_of(lni, struct cxi_lni_priv, lni);
+	struct cxi_dev *cdev = lni_priv->dev;
+
+	/* We do not free physical LACs for VFs. They will be freed when the
+	 * LNI is freed.
+	 */
+	if (!cdev->is_physfn)
+		return;
 
 	cass_lac_free(lni_priv, lac);
 }

drivers/net/ethernet/hpe/ss1/cass_cpt.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0
-/* Copyright 2018 Hewlett Packard Enterprise Development LP */
+/* Copyright 2018, 2024-2026 Hewlett Packard Enterprise Development LP */
 
 /* Communication Profile Table (CPT) management */
 
@@ -235,6 +235,67 @@ static struct cass_cp *cass_cp_get(struct cxi_tx_profile *tx_profile,
 }
 
 /**
+ * cxi_trig_cp_alloc_vf() - Allocate a communication profile with type of
+ *                          LCID parameter for VF
+ *
+ * @lni: LNI the communication profile should be mapped into
+ * @vni_pcp: VNI of communication profile. If using an ethernet tc
+ * this argument is treated as PCP.
+ * @tc: Traffic class of the communication profile
+ * @tc_type: Traffic class type of the communication profile
+ * @cp_type: NON_TRIG_LCID and any LCID for ANY_LCID.
+ *
+ * Note: Allocating communication profiles with the same LNI, VNI, TC, and TC
+ * type will result in a redundant communication profile being mapped into the
+ * LNI.
+ *
+ * @return: Valid pointer on success. Else, negative errno value.
+ */
+static struct cxi_cp *cxi_trig_cp_alloc_vf(struct cxi_lni *lni, unsigned int vni_pcp,
+					   unsigned int tc,
+					   enum cxi_traffic_class_type tc_type,
+					   enum cxi_trig_cp cp_type)
+{
+	struct cxi_lni_priv_vf *lni_priv_vf =
+		container_of(lni, struct cxi_lni_priv_vf, lni);
+	struct cxi_dev *dev = lni_priv_vf->dev;
+	struct cxi_cp_priv_vf *cp_priv_vf;
+	const struct cxi_trig_cp_alloc_cmd cmd = {
+		.op = CXI_OP_TRIG_CP_ALLOC,
+		.lni = lni_priv_vf->lni.id,
+		.vni = vni_pcp,
+		.tc = tc,
+		.tc_type = tc_type,
+		.cp_type = cp_type
+	};
+	struct cxi_cp_alloc_resp resp;
+	size_t resp_len = sizeof(resp);
+	int rc;
+
+	cp_priv_vf = kzalloc(sizeof(*cp_priv_vf), GFP_KERNEL);
+	if (!cp_priv_vf)
+		return ERR_PTR(-ENOMEM);
+
+	rc = cxi_send_msg_to_pf(dev, &cmd, sizeof(cmd), &resp, &resp_len);
+	if (rc)
+		goto error_out;
+
+	cp_priv_vf->dev = dev;
+	cp_priv_vf->cp_hndl = resp.cp_hndl;
+	cp_priv_vf->cp.vni_pcp = vni_pcp;
+	cp_priv_vf->cp.tc = tc;
+	cp_priv_vf->cp.tc_type = tc_type;
+	cp_priv_vf->cp.lcid = resp.lcid;
+
+	return &cp_priv_vf->cp;
+
+error_out:
+	kfree(cp_priv_vf);
+
+	return ERR_PTR(rc);
+}
+
+/**
  * cxi_trig_cp_alloc() - Allocate a communication profile with type of
  *                       LCID parameter
  *
@@ -244,7 +305,7 @@ static struct cass_cp *cass_cp_get(struct cxi_tx_profile *tx_profile,
  * @tc: Traffic class of the communication profile
  * @tc_type: Traffic class type of the communication profile
  * @cp_type: Return CP with LCID 0 for TRIG_LCID, a non-0 LCID for
- *.          NON_TRIG_LCID and any LCID for ANY_LCID.
+ *           NON_TRIG_LCID and any LCID for ANY_LCID.
  *
  * Note: Allocating communication profiles with the same LNI, VNI, TC, and TC
  * type will result in a redundant communication profile being mapped into the
@@ -266,6 +327,9 @@ struct cxi_cp *cxi_trig_cp_alloc(struct cxi_lni *lni, unsigned int vni_pcp,
 	struct cxi_tx_profile *tx_profile = NULL;
 	int rc;
 	int lcid;
+
+	if (!dev->is_physfn)
+		return cxi_trig_cp_alloc_vf(lni, vni_pcp, tc, tc_type, cp_type);
 
 	if (tc < 0 || tc >= CXI_ETH_TC_MAX || tc_type < 0 ||
 	    tc_type >= CXI_TC_TYPE_MAX)
@@ -319,6 +383,7 @@ struct cxi_cp *cxi_trig_cp_alloc(struct cxi_lni *lni, unsigned int vni_pcp,
 		return ERR_PTR(-ENOMEM);
 	}
 
+	cp_priv->dev = dev;
 	cp_priv->rgid = lni_priv->lni.rgid;
 	cp_priv->lni_id = lni_priv->lni.id;
 	cp_priv->cp.vni_pcp = vni_pcp;
@@ -393,6 +458,30 @@ struct cxi_cp *cxi_cp_alloc(struct cxi_lni *lni, unsigned int vni_pcp,
 EXPORT_SYMBOL(cxi_cp_alloc);
 
 /**
+ * cxi_cp_free_vf() - Free the communication profile for VF
+ *
+ * @cp: Communication profile to be freed
+ */
+static void cxi_cp_free_vf(struct cxi_cp *cp)
+{
+	struct cxi_cp_priv_vf *cp_priv_vf =
+		container_of(cp, struct cxi_cp_priv_vf, cp);
+	struct cxi_dev *dev = cp_priv_vf->dev;
+	const struct cxi_cp_free_cmd cmd = {
+		.op = CXI_OP_CP_FREE,
+		.cp_hndl = cp_priv_vf->cp_hndl,
+	};
+	size_t resp_len = 0;
+	int rc;
+
+	rc = cxi_send_msg_to_pf(dev, &cmd, sizeof(cmd), NULL, &resp_len);
+	if (rc)
+		return;
+
+	kfree(cp_priv_vf);
+}
+
+/**
  * cxi_cp_free() - Free the communication profile
  *
  * @cp: Communication profile to be freed
@@ -400,8 +489,16 @@ EXPORT_SYMBOL(cxi_cp_alloc);
 void cxi_cp_free(struct cxi_cp *cp)
 {
 	struct cxi_cp_priv *cp_priv = container_of(cp, struct cxi_cp_priv, cp);
-	struct cass_dev *hw = cp_priv->cass_cp->hw;
-	struct cxi_tx_profile *tx_profile = cp_priv->cass_cp->tx_profile;
+	struct cass_dev *hw;
+	struct cxi_tx_profile *tx_profile;
+
+	if (!cp_priv->dev->is_physfn) {
+		cxi_cp_free_vf(cp);
+		return;
+	}
+
+	hw = cp_priv->cass_cp->hw;
+	tx_profile = cp_priv->cass_cp->tx_profile;
 
 	cxi_tx_profile_dec_refcount(&hw->cdev, tx_profile, true);
 

drivers/net/ethernet/hpe/ss1/cass_dmac.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0
-/* Copyright 2020-2021 Hewlett Packard Enterprise Development LP */
+/* Copyright 2020-2021, 2024-2026 Hewlett Packard Enterprise Development LP */
 
 #include <linux/iopoll.h>
 #include <linux/bitmap.h>
@@ -72,6 +72,9 @@ int cxi_dmac_desc_set_reset(struct cxi_dev *cdev, int set_id)
 	struct cass_dev *hw = container_of(cdev, struct cass_dev, cdev);
 	int retval;
 
+	if (!cdev->is_physfn)
+		return -EPERM;
+
 	mutex_lock(&hw->dmac.lock);
 
 	retval = cass_dmac_desc_set_id_verify(hw, set_id);
@@ -109,6 +112,9 @@ int cxi_dmac_desc_set_add(struct cxi_dev *cdev, int set_id, dma_addr_t dst,
 	union c_pi_cfg_dmac_desc desc = {};
 	u32 src_end;
 	const u32 u64count = len / sizeof(u64);
+
+	if (!cdev->is_physfn)
+		return -EPERM;
 
 	/*
 	 * dst address and src offset must be 64-bit aligned
@@ -313,6 +319,9 @@ int cxi_dmac_desc_set_reserve(struct cxi_dev *cdev, u16 num_descs,
 	struct cass_dev *hw = container_of(cdev, struct cass_dev, cdev);
 	int retval;
 
+	if (!cdev->is_physfn)
+		return -EPERM;
+
 	cxidev_dbg(cdev, "hw=%p num_descs=%u desc_idx=%u\n", hw, num_descs,
 		   desc_idx);
 
@@ -342,6 +351,9 @@ int cxi_dmac_desc_set_alloc(struct cxi_dev *cdev, u16 num_descs,
 	struct cass_dev *hw = container_of(cdev, struct cass_dev, cdev);
 	int retval;
 
+	if (!cdev->is_physfn)
+		return -EPERM;
+
 	cxidev_dbg(cdev, "hw=%p num_descs=%u\n", hw, num_descs);
 
 	retval = cass_dmac_desc_set_common_alloc(hw, num_descs,
@@ -368,6 +380,9 @@ int cxi_dmac_desc_set_free(struct cxi_dev *cdev, int set_id)
 	int retval = 0;
 	u16 count;
 	u16 index;
+
+	if (!cdev->is_physfn)
+		return -EPERM;
 
 	cxidev_dbg(cdev, "hw=%p set_id=%d\n", hw, set_id);
 
@@ -403,6 +418,9 @@ int cxi_dmac_xfer(struct cxi_dev *cdev, int set_id)
 	struct cass_dev *hw = container_of(cdev, struct cass_dev, cdev);
 	volatile struct c_pi_dmac_cdesc *cpl_desc = hw->dmac.cpl_desc;
 	int retval;
+
+	if (!cdev->is_physfn)
+		return -EPERM;
 
 	cxidev_dbg(&hw->cdev, "hw=%p set_id=%d\n", hw, set_id);
 

drivers/net/ethernet/hpe/ss1/cass_lni.c
@@ -1,11 +1,13 @@
 // SPDX-License-Identifier: GPL-2.0
-/* Copyright 2018 Hewlett Packard Enterprise Development LP */
+/* Copyright 2018, 2024-2026 Hewlett Packard Enterprise Development LP */
 
 /* Cassini NI management */
 
 #include <linux/types.h>
 
 #include "cass_core.h"
+#include "cxi_internal.h"
+#include "cxi_vf_cmd.h"
 #include "cass_ss1_debugfs.h"
 
 unsigned int cxi_lni_get_pe_num(struct cxi_lni_priv *lni)
@@ -25,12 +27,75 @@ unsigned int cxi_lni_get_pe_num(struct cxi_lni_priv *lni)
 }
 
 /**
- * cxi_lni_alloc() - Allocate a logical NI (LNI) on a device.
+ * cxi_lni_alloc_vf() - Allocate a logical NI (LNI) for VF
  *
  * @dev: CXI device
  * @svc_id: Service ID of service to be used. May be CXI_DEFAULT_SVC_ID.
+ *
+ * Return: Pointer to allocated LNI on success, ERR_PTR on failure.
  */
-struct cxi_lni *cxi_lni_alloc(struct cxi_dev *dev, unsigned int svc_id)
+static struct cxi_lni *cxi_lni_alloc_vf(struct cxi_dev *dev,
+					unsigned int svc_id)
+{
+	struct cass_dev *hw = container_of(dev, struct cass_dev, cdev);
+	struct cxi_lni_priv_vf *lni_priv_vf;
+	const struct cxi_lni_alloc_cmd lni_alloc_cmd = {
+		.op = CXI_OP_LNI_ALLOC,
+		.svc_id = svc_id,
+	};
+	struct cxi_phys_lac_alloc_cmd lac_alloc_cmd = {
+		.op = CXI_OP_PHYS_LAC_ALLOC,
+		.resp = NULL,
+	};
+	struct cxi_lni_alloc_resp_vf lni_alloc_resp;
+	struct cxi_phys_lac_alloc_resp lac_alloc_resp;
+	size_t resp_len;
+	int rc;
+
+	lni_priv_vf = kzalloc(sizeof(*lni_priv_vf), GFP_KERNEL);
+	if (!lni_priv_vf)
+		return ERR_PTR(-ENOMEM);
+
+	resp_len = sizeof(lni_alloc_resp);
+	rc = cxi_send_msg_to_pf(&hw->cdev, &lni_alloc_cmd, sizeof(lni_alloc_cmd),
+				&lni_alloc_resp, &resp_len);
+	if (rc)
+		goto error_out;
+
+	lni_priv_vf->dev = dev;
+	lni_priv_vf->lni.id = lni_alloc_resp.base.lni;
+	lni_priv_vf->lni.rgid = lni_alloc_resp.rgid;
+
+	/* Allocate Physical LAC for this LNI */
+	resp_len = sizeof(lac_alloc_resp);
+	lac_alloc_cmd.lni = lni_priv_vf->lni.id;
+	rc = cxi_send_msg_to_pf(&hw->cdev, &lac_alloc_cmd, sizeof(lac_alloc_cmd),
+				&lac_alloc_resp, &resp_len);
+	if (rc) {
+		lni_priv_vf->phys_lac = -1;
+		cxi_lni_free(&lni_priv_vf->lni);
+		return ERR_PTR(rc);
+	}
+	lni_priv_vf->phys_lac = lac_alloc_resp.lac;
+
+	return &lni_priv_vf->lni;
+
+error_out:
+	kfree(lni_priv_vf);
+
+	return ERR_PTR(rc);
+}
+
+/**
+ * cxi_lni_alloc_internal() - Allocate a logical NI (LNI) internal function
+ *
+ * @dev: CXI device
+ * @svc_id: Service ID of service to be used. May be CXI_DEFAULT_SVC_ID.
+ * @vf_en: true if this LNI is for a VF
+ * @vf_num: VF number (ignored unless vf_en is true)
+ */
+struct cxi_lni *cxi_lni_alloc_internal(struct cxi_dev *dev, unsigned int svc_id,
+				       bool vf_en, u8 vf_num)
 {
 	struct cass_dev *hw = container_of(dev, struct cass_dev, cdev);
 	struct cxi_lni_priv *lni_priv;
@@ -74,6 +139,14 @@ struct cxi_lni *cxi_lni_alloc(struct cxi_dev *dev, unsigned int svc_id)
 		goto free_lni_id;
 	}
 
+	lni_priv->is_vf = vf_en;
+	if (vf_en) {
+		lni_priv->vf_num = vf_num;
+		lni_priv->phys_ac = hw->vfs[vf_num].phys_ac;
+	} else {
+		lni_priv->phys_ac = ATU_PHYS_AC;
+	}
+
 	lni_priv->rgroup = rgroup;
 	lni_priv->dev = dev;
 	lni_priv->lni.id = id;
@@ -115,6 +188,19 @@ put_rgid:
 dec_svc:
 	cxi_rgroup_dec_refcount(rgroup);
 	return err;
+}
+EXPORT_SYMBOL(cxi_lni_alloc_internal);
+
+/**
+ * cxi_lni_alloc() - Allocate a logical NI (LNI)
+ *
+ * @dev: CXI device
+ * @svc_id: Service ID of service to be used. May be CXI_DEFAULT_SVC_ID.
+ */
+struct cxi_lni *cxi_lni_alloc(struct cxi_dev *dev, unsigned int svc_id)
+{
+	return dev->is_physfn ? cxi_lni_alloc_internal(dev, svc_id, false, 0) :
+				cxi_lni_alloc_vf(dev, svc_id);
 }
 EXPORT_SYMBOL(cxi_lni_alloc);
 
@@ -197,6 +283,45 @@ void lni_cleanups_work(struct work_struct *work)
 }
 
 /**
+ * cxi_lni_free_vf() - Shutdown and release an LNI for a VF.
+ *
+ * @lni: LNI to release
+ */
+static int cxi_lni_free_vf(struct cxi_lni *lni)
+{
+	struct cxi_lni_priv_vf *lni_priv_vf =
+		container_of(lni, struct cxi_lni_priv_vf, lni);
+	struct cxi_dev *dev = lni_priv_vf->dev;
+	const struct cxi_lni_free_cmd lni_free_cmd = {
+		.op = CXI_OP_LNI_FREE,
+		.lni = lni_priv_vf->lni.id,
+	};
+	const struct cxi_phys_lac_free_cmd lac_free_cmd = {
+		.op = CXI_OP_PHYS_LAC_FREE,
+		.lni = lni_priv_vf->lni.id,
+		.lac = lni_priv_vf->phys_lac,
+	};
+	size_t resp_len;
+	int rc;
+
+	/* Free the physical LAC */
+	if (lni_priv_vf->phys_lac >= 0) {
+		resp_len = 0;
+		cxi_send_msg_to_pf(dev, &lac_free_cmd, sizeof(lac_free_cmd), NULL, &resp_len);
+	}
+
+	/* Free LNI */
+	resp_len = 0;
+	rc = cxi_send_msg_to_pf(dev, &lni_free_cmd, sizeof(lni_free_cmd), NULL, &resp_len);
+	if (rc)
+		return rc;
+
+	kfree(lni_priv_vf);
+
+	return 0;
+}
+
+/**
  * cxi_lni_free() - Shutdown and release an LNI.
  *
  * @lni: LNI to release
@@ -207,6 +332,9 @@ int cxi_lni_free(struct cxi_lni *lni)
 		container_of(lni, struct cxi_lni_priv, lni);
 	struct cxi_dev *dev = lni_priv->dev;
 	struct cass_dev *hw = container_of(dev, struct cass_dev, cdev);
+
+	if (!hw->cdev.is_physfn)
+		return cxi_lni_free_vf(lni);
 
 	spin_lock(&hw->lni_lock);
 	list_del(&lni_priv->list);

drivers/net/ethernet/hpe/ss1/cass_nta.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0
-/* Copyright 2019 Hewlett Packard Enterprise Development LP */
+/* Copyright 2019, 2024-2026 Hewlett Packard Enterprise Development LP */
 
 /*
  * Address Translation Unit (ATU) management
@@ -545,6 +545,15 @@ done:
 	return rc;
 }
 
+static int cxi_inbound_wait_vf(struct cxi_dev *cdev)
+{
+	const struct cxi_inbound_wait_cmd cmd = {
+		.op = CXI_OP_INBOUND_WAIT,
+	};
+
+	return cxi_send_msg_to_pf(cdev, &cmd, sizeof(cmd), NULL, 0);
+}
+
 extern bool ibw_epoch_cntr_is_0(struct cass_dev *hw, bool *epoch_ret);
 /*
  * cxi_inbound_wait - Flush internal cassini buffers
@@ -557,6 +566,9 @@ int cxi_inbound_wait(struct cxi_dev *cdev)
 	int ret;
 	struct cass_dev *hw = container_of(cdev, struct cass_dev, cdev);
 	struct cass_atu_cq *cq = &hw->atu_cq;
+
+	if (!cdev->is_physfn)
+		return cxi_inbound_wait_vf(cdev);
 
 	mutex_lock(&cq->atu_ib_mutex);
 	ret = cass_inbound_wait(hw, true);

drivers/net/ethernet/hpe/ss1/cass_sbus.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0
-/* Copyright 2020,2022 Hewlett Packard Enterprise Development LP */
+/* Copyright 2020, 2022, 2024-2026 Hewlett Packard Enterprise Development LP */
 
 /* Cassini SBUS driver */
 
@@ -26,6 +26,9 @@ int cxi_sbus_op_reset(struct cxi_dev *cdev)
 	union c_mb_dbg_cfg_sbus_master cfg = {};
 	union c_mb_dbg_sts_sbus_master sts;
 	int rc;
+
+	if (!cdev->is_physfn)
+		return -EPERM;
 
 	rc = mutex_lock_interruptible(&hw->sbus_mutex);
 	if (rc) {
@@ -80,6 +83,9 @@ int cxi_sbus_op(struct cxi_dev *cdev, const struct cxi_sbus_op_params *params,
 	union c_mb_dbg_sts_sbus_master sts;
 	void __iomem *sts_csr = cass_csr(hw, C_MB_DBG_STS_SBUS_MASTER);
 	int rc;
+
+	if (!cdev->is_physfn)
+		return -EPERM;
 
 	if (!rsp_data || !result_code || !overrun)
 		return -EINVAL;
@@ -151,6 +157,9 @@ int cxi_serdes_op(struct cxi_dev *cdev, u64 serdes_sel, u64 op, u64 data,
 		  int timeout, unsigned int flags, u16 *result)
 {
 	struct cass_dev *hw = container_of(cdev, struct cass_dev, cdev);
+
+	if (!cdev->is_physfn)
+		return -EPERM;
 
 	if (!cass_version(hw, CASSINI_1)) {
 		cxidev_err(cdev, "%s NOT IMPLEMENTED!\n", __func__);

drivers/net/ethernet/hpe/ss1/cass_sriov.c
@@ -349,6 +349,9 @@ err:
 
 	vf->kvm_task = NULL;
 
+	cass_ac_phys_free(hw, vf->phys_ac);
+	vf->phys_ac = 0;
+
 	return rc;
 }
 
@@ -471,11 +474,21 @@ static void handle_vf_req_conn(struct cass_dev *hw,
 	vf->req_sock = incoming;
 	get_random_bytes(&vf->token, sizeof(vf->token));
 
+	rc = cass_ac_phys_alloc(hw, true, vf_idx);
+	if (rc <= 0) {
+		cxidev_err(&hw->cdev, "could not allocate ac for vf %d: %d",
+			   vf_idx, rc);
+		vf->req_sock = NULL;
+		goto close_sock;
+	}
+	vf->phys_ac = rc;
 	cxidev_dbg(&hw->cdev, "allocated acid %d for vf %d", vf->phys_ac, vf_idx);
 
 	vf->task = kthread_run(pf_vf_msghandler, vf, "cxi_vf_%d", vf_idx);
 	if (IS_ERR(vf->task)) {
 		cxidev_err(&hw->cdev, "failed to start handler for vf %d", vf_idx);
+		cass_ac_phys_free(hw, vf->phys_ac);
+		vf->phys_ac = 0;
 		vf->req_sock = NULL;
 		vf->task = NULL;
 		goto close_sock;

commit bfe2d6c8a5de1c96a2a395363b660d7b34b8a572

Regression: Fix ida_simple_* use
The upstream 6.18 kernel completely removed ida_simple_* helpers.
Change 043743cd7250 (Replace ida_simple_xxx calls) fixed that issue.
However, change 938633a4e81d (sriov: Add VF support for LNI, services,
 and communication profiles) re-introduced the issue.  This patch
corrects that.

Fixes: 938633a4e81d
Signed-off-by: Doug Ledford <doug.ledford@hpe.com>

drivers/net/ethernet/hpe/ss1/cass_atu.c
@@ -399,7 +399,7 @@ int cass_ac_phys_alloc(struct cass_dev *hw, bool vf_en, int vf_num)
 		.ats_vf_num = vf_en ? vf_num : 0,
 	};
 
-	acid = ida_simple_get(&hw->atu_table, 1, ATU_PHYS_AC, GFP_KERNEL);
+	acid = ida_alloc_range(&hw->atu_table, 1, ATU_PHYS_AC - 1, GFP_KERNEL);
 	if (acid < 0)
 		return acid;
 
@@ -420,7 +420,7 @@ void cass_ac_phys_free(struct cass_dev *hw, int acid)
 	union c_atu_cfg_ac_table ac = {};
 
 	cass_write_ac(hw, &ac, acid, false, true, false, 0);
-	ida_simple_remove(&hw->atu_table, acid);
+	ida_free(&hw->atu_table, acid);
 }
 
 /**